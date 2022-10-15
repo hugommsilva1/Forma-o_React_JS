a3b2d2cc8fd305a0e107d0d20f80d33a506f2438
hugommsilva1 <64027942+hugommsilva1@users.noreply.github.com>
2022-10-15 13:40:19 +0100

    Change App.js

--- projetohugo/src/App.js
+++ projetohugo/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Formação : Feedback app
+          Formação : Lista de Compras
         </a>
       </header>
     </div>
