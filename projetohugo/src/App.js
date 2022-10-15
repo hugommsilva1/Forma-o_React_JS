5981bfcfb9ffd5add83828cd06e0bcb36c2b9239
hugommsilva1 <64027942+hugommsilva1@users.noreply.github.com>
2022-10-15 13:26:45 +0100

    Update App.js

--- projetohugo/src/App.js
+++ projetohugo/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Formação : Feedback app
         </a>
       </header>
     </div>
